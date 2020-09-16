2e51bdf49b767c1dbb5fe4efd2dbca64cd19b7f0
Davide Cesare <davidereaper@gmail.com>
2020-09-16 15:34:34 +0200

    Added generation row and square

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+  var sourceRow = $("#temp_row").html();
+  var templateRow = Handlebars.compile(sourceRow);
+  for (var i = 0; i < 6; i++) {
+    var visualRow = templateRow();
+    $(".container").append(visualRow);
+  }
+
+  var sourceSquare = $("#temp_square").html();
+  var templateSquare = Handlebars.compile(sourceSquare);
+  for (var i = 0; i < 6; i++) {
+    var visualSquare = templateSquare();
+    $(".row").append(visualSquare);
+  }
+
   $(".square").click(
     function() {
       var endpoint = "https://flynn.boolean.careers/exercises/api/random/int";
